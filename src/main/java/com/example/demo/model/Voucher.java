3d4ae2e11fd5f1e6908f37d17e932ca558a7e34f
phat2007ne <phatdttc00351@fpt.edu.vn>
2026-01-18 12:21:32 +0700

    Khuyenmai

---
+++ src/main/java/com/example/demo/model/Voucher.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.*; // Nhập hết các annotation
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "vouchers")
+public class Voucher {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(nullable = false, unique = true)
+    String code;
+
+    @Column(name = "discount_percent")
+    Integer discountPercent;
+
+    @Column(name = "discount_amount")
+    Double discountAmount; 
+
+    @Column(name = "min_order_value")
+    Double minOrderValue;
+
+    @Column(name = "expired_at")
+    LocalDateTime expiredAt;
+
+    Boolean active;
+
+    // --- THÊM PHẦN NÀY ---
+    // Nếu account null -> Ai dùng cũng được (Voucher chung)
+    // Nếu account có dữ liệu -> Chỉ người này được dùng
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    Account account; 
+}
