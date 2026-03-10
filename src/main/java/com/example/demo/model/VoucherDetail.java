bc585f6432d6e4d457d2a3d6e59c2806c2657841
Long-crypto123 <buihoangkimlong70@gmail.com>
2026-03-10 19:16:30 +0700

    thêm vocherdetail

---
+++ src/main/java/com/example/demo/model/VoucherDetail.java
@@ -0,0 +1,52 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "vouchers_detail")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VoucherDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @ManyToOne
+    @JoinColumn(name = "vouchers_id")
+    private Voucher voucher;
+
+    @Column(name = "is_used")
+    private Boolean isUsed;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "used_at")
+    private Date usedAt;
+
+    private String status;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "received_at")
+    private Date receivedAt;
+    
+
+}
