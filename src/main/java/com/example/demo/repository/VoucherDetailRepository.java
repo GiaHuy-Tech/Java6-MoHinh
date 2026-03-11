6022334578a16196e672e20a16ebd7622510704f
Long-crypto123 <buihoangkimlong70@gmail.com>
2026-03-12 01:03:34 +0700

    thêm bình luận vào product-detail

--- src/main/java/com/example/demo/repository/VoucherDetailRepository.java
+++ src/main/java/com/example/demo/repository/VoucherDetailRepository.java
@@ -7,29 +7,10 @@ import com.example.demo.model.VoucherDetail;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface VoucherDetailRepository extends JpaRepository<VoucherDetail, Long> {
 
 	// Tìm voucher chi tiết của account dựa vào mã voucher, đảm bảo chưa sử dụng
-<<<<<<< Updated upstream
-	@Query("SELECT vd FROM VoucherDetail vd " + "JOIN FETCH vd.voucher v " + "WHERE vd.account.id = :accountId "
-			+ "AND v.code = :code " + "AND vd.isUsed = false " + "AND v.active = true")
-	Optional<VoucherDetail> findValidVoucherForAccount(@Param("accountId") Integer accountId,
-			@Param("code") String code);
-
-	List<VoucherDetail> findByAccount_Id(Integer accountId);
-
-	@Query("""
-			SELECT vd FROM VoucherDetail vd
-			JOIN FETCH vd.voucher v
-			WHERE vd.account.id = :accountId
-			AND vd.isUsed = false
-			AND v.active = true
-			AND v.expiredAt > :now
-			""")
-	List<VoucherDetail> findAvailableVouchers(@Param("accountId") Integer accountId, @Param("now") LocalDateTime now);
-=======
 	// Lấy các voucher khả dụng của user
 	@Query("""
 			    SELECT vd FROM VoucherDetail vd
@@ -47,5 +28,4 @@ public interface VoucherDetailRepository extends JpaRepository<VoucherDetail, Lo
 	// Lấy voucher chưa dùng
 	List<VoucherDetail> findByAccountIdAndIsUsedFalse(Integer accountId);
 
->>>>>>> Stashed changes
 }
